84d1713e945940661e5b70912a4de3b3a2e39eb2
Robin <robin@muntafu.eu>
2024-05-01 22:02:11 +0200

    removed unnused variables and added wifi connection

--- main/rest_server.c
+++ main/rest_server.c
@@ -36,109 +36,6 @@ typedef struct rest_server_context {
 
 #define CHECK_FILE_EXTENSION(filename, ext) (strcasecmp(&filename[strlen(filename) - strlen(ext)], ext) == 0)
 
-/* Set HTTP response content type according to file extension */
-static esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filepath)
-{
-    const char *type = "text/plain";
-    if (CHECK_FILE_EXTENSION(filepath, ".html")) {
-        type = "text/html";
-    } else if (CHECK_FILE_EXTENSION(filepath, ".js")) {
-        type = "application/javascript";
-    } else if (CHECK_FILE_EXTENSION(filepath, ".css")) {
-        type = "text/css";
-    } else if (CHECK_FILE_EXTENSION(filepath, ".png")) {
-        type = "image/png";
-    } else if (CHECK_FILE_EXTENSION(filepath, ".ico")) {
-        type = "image/x-icon";
-    } else if (CHECK_FILE_EXTENSION(filepath, ".svg")) {
-        type = "text/xml";
-    }
-    return httpd_resp_set_type(req, type);
-}
-
-/* Send HTTP response with the contents of the requested file */
-static esp_err_t rest_common_get_handler(httpd_req_t *req)
-{
-    char filepath[FILE_PATH_MAX];
-
-    rest_server_context_t *rest_context = (rest_server_context_t *)req->user_ctx;
-    strlcpy(filepath, rest_context->base_path, sizeof(filepath));
-    if (req->uri[strlen(req->uri) - 1] == '/') {
-        strlcat(filepath, "/index.html", sizeof(filepath));
-    } else {
-        strlcat(filepath, req->uri, sizeof(filepath));
-    }
-    int fd = open(filepath, O_RDONLY, 0);
-    if (fd == -1) {
-        ESP_LOGE(REST_TAG, "Failed to open file : %s", filepath);
-        /* Respond with 500 Internal Server Error */
-        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to read existing file");
-        return ESP_FAIL;
-    }
-
-    set_content_type_from_file(req, filepath);
-
-    char *chunk = rest_context->scratch;
-    ssize_t read_bytes;
-    do {
-        /* Read file in chunks into the scratch buffer */
-        read_bytes = read(fd, chunk, SCRATCH_BUFSIZE);
-        if (read_bytes == -1) {
-            ESP_LOGE(REST_TAG, "Failed to read file : %s", filepath);
-        } else if (read_bytes > 0) {
-            /* Send the buffer contents as HTTP response chunk */
-            if (httpd_resp_send_chunk(req, chunk, read_bytes) != ESP_OK) {
-                close(fd);
-                ESP_LOGE(REST_TAG, "File sending failed!");
-                /* Abort sending file */
-                httpd_resp_sendstr_chunk(req, NULL);
-                /* Respond with 500 Internal Server Error */
-                httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to send file");
-                return ESP_FAIL;
-            }
-        }
-    } while (read_bytes > 0);
-    /* Close file after sending complete */
-    close(fd);
-    ESP_LOGI(REST_TAG, "File sending complete");
-    /* Respond with an empty chunk to signal HTTP response completion */
-    httpd_resp_send_chunk(req, NULL, 0);
-    return ESP_OK;
-}
-
-/* Simple handler for light brightness control */
-static esp_err_t light_brightness_post_handler(httpd_req_t *req)
-{
-    int total_len = req->content_len;
-    int cur_len = 0;
-    char *buf = ((rest_server_context_t *)(req->user_ctx))->scratch;
-    int received = 0;
-    if (total_len >= SCRATCH_BUFSIZE) {
-        /* Respond with 500 Internal Server Error */
-        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "content too long");
-        return ESP_FAIL;
-    }
-    while (cur_len < total_len) {
-        received = httpd_req_recv(req, buf + cur_len, total_len);
-        if (received <= 0) {
-            /* Respond with 500 Internal Server Error */
-            httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to post control value");
-            return ESP_FAIL;
-        }
-        cur_len += received;
-    }
-    buf[total_len] = '\0';
-
-    cJSON *root = cJSON_Parse(buf);
-    int red = cJSON_GetObjectItem(root, "red")->valueint;
-    int green = cJSON_GetObjectItem(root, "green")->valueint;
-    int blue = cJSON_GetObjectItem(root, "blue")->valueint;
-    ESP_LOGI(REST_TAG, "Light control: red = %d, green = %d, blue = %d", red, green, blue);
-    cJSON_Delete(root);
-    httpd_resp_sendstr(req, "Post control value successfully");
-    return ESP_OK;
-}
-
 /* Simple handler for getting system handler */
 static esp_err_t system_info_get_handler(httpd_req_t *req)
 {
@@ -165,6 +62,40 @@ static esp_err_t temperature_data_get_handler(httpd_req_t *req)
     httpd_resp_sendstr(req, sys_info);
     free((void *)sys_info);
     cJSON_Delete(root);
+    return ESP_OK;
+}
+
+static esp_err_t swimming_pool_get_receiped(httpd_req_t *req) {
+    httpd_resp_set_type(req, "application/json");
+
+    // Create the root of the JSON object
+    cJSON *root = cJSON_CreateObject();
+
+    // Adding the recipe name
+    cJSON_AddStringToObject(root, "name", "Swimming Pool");
+
+    // Adding the ingredients as an array
+    cJSON *ingredients = cJSON_AddArrayToObject(root, "ingredients");
+    cJSON_AddItemToArray(ingredients, cJSON_CreateString("30ml Vodka"));
+    cJSON_AddItemToArray(ingredients, cJSON_CreateString("30ml Malibu"));
+    cJSON_AddItemToArray(ingredients, cJSON_CreateString("20ml Cream of Coconut"));
+    cJSON_AddItemToArray(ingredients, cJSON_CreateString("120ml Pineapple Juice"));
+    cJSON_AddItemToArray(ingredients, cJSON_CreateString("20ml Blue Curaçao"));
+    cJSON_AddItemToArray(ingredients, cJSON_CreateString("Ice cubes"));
+
+    // Adding the preparation steps
+    cJSON_AddStringToObject(root, "preparation", "Fill a shaker with ice cubes. Add vodka, cream of coconut, pineapple juice, blue curaçao, and sweet and sour mix. Shake well. Strain into a chilled highball glass filled with ice cubes. Garnish with a slice of pineapple and a cherry.");
+
+    // Convert the entire JSON object into a string
+    char *response_string = cJSON_Print(root);
+
+    // Send the JSON response
+    httpd_resp_send(req, response_string, HTTPD_RESP_USE_STRLEN);
+
+    // Free the JSON object
+    cJSON_Delete(root);
+    free(response_string);
+
     return ESP_OK;
 }
 
@@ -200,23 +131,14 @@ esp_err_t start_rest_server(const char *base_path)
     };
     httpd_register_uri_handler(server, &temperature_data_get_uri);
 
-    /* URI handler for light brightness control */
-    httpd_uri_t light_brightness_post_uri = {
-        .uri = "/api/v1/light/brightness",
-        .method = HTTP_POST,
-        .handler = light_brightness_post_handler,
-        .user_ctx = rest_context
-    };
-    httpd_register_uri_handler(server, &light_brightness_post_uri);
-
-    /* URI handler for getting web server files */
-    httpd_uri_t common_get_uri = {
-        .uri = "/*",
+        /* URI handler for fetching temperature data */
+    httpd_uri_t swimmingpool_receipe_get_uri = {
+        .uri = "/api/v1/cocktail/swimmingpool",
         .method = HTTP_GET,
-        .handler = rest_common_get_handler,
+        .handler = swimming_pool_get_receiped,
         .user_ctx = rest_context
     };
-    httpd_register_uri_handler(server, &common_get_uri);
+    httpd_register_uri_handler(server, &swimmingpool_receipe_get_uri);
 
     return ESP_OK;
 err_start:
---
+++ main/wifi.h
@@ -0,0 +1,13 @@
+#ifndef __wifi_h__
+#define __wifi_h__
+
+/**
+ * @brief connect to wifi
+ * 
+ * @param[in] ssid the ssid of the wifi
+ * 
+ * @param[in] pass the password of the wifi
+*/
+void wifiConnect(const char *ssid, const char *pass);
+
+#endif
